234c73e729fec1ed22b190cd7b3a11b27854c07a
T25 <106699712+tirso25@users.noreply.github.com>
2024-06-16 03:38:38 +0200

    Update temas.sh

--- temas.sh
+++ temas.sh
@@ -1,18 +1,61 @@
 #!/bin/bash
 
 # URLs de los archivos en el repositorio de GitHub
+URL_FONT="https://github.com/tirso25/dotfiles/raw/main/FiraCode-VariableFont_wght.ttf"
 URL_THEME="https://github.com/tirso25/dotfiles/raw/main/Nordic-darker.tar.xz"
 URL_ICONS="https://github.com/tirso25/dotfiles/raw/main/Tela-black.tar.xz"
 URL_DRACULA_ICONS="https://github.com/tirso25/dotfiles/raw/main/dracula-icons-main.zip"
 
 # Directorios destino
+DOWNLOAD_DIR="$HOME/Descargas"
+FONT_DIR="$HOME/.local/share/fonts"
 DEST_DIR_THEME="/usr/share/themes"
 DEST_DIR_ICONS="/usr/share/icons"
 
 # Nombres de los archivos descargados
+FONT_FILE="FiraCode-VariableFont_wght.ttf"
 FILE_NAME_THEME="Nordic-darker.tar.xz"
 FILE_NAME_ICONS="Tela-black.tar.xz"
 FILE_NAME_DRACULA_ICONS="dracula-icons-main.zip"
+
+# Función para descargar y mover fuentes
+download_and_move_font() {
+    local url=$1
+    local download_dir=$2
+    local font_dir=$3
+    local font_file=$4
+
+    # Crear directorio de descargas si no existe
+    mkdir -p "$download_dir"
+
+    # Descargar la fuente
+    echo "Descargando la fuente desde $url..."
+    wget -q --show-progress -O "$download_dir/$font_file" "$url"
+
+    if [ $? -ne 0 ]; then
+        echo "Error: No se pudo descargar la fuente desde $url."
+        exit 1
+    fi
+
+    # Crear directorio de fuentes locales si no existe
+    mkdir -p "$font_dir"
+
+    # Mover la fuente al directorio de fuentes locales
+    echo "Moviendo la fuente a $font_dir..."
+    cp "$download_dir/$font_file" "$font_dir"
+
+    # Actualizar la caché de fuentes
+    echo "Actualizando la caché de fuentes..."
+    fc-cache -f -v
+
+    # Verificar la instalación
+    if fc-list | grep "$font_file"; then
+        echo "Fuente instalada correctamente."
+    else
+        echo "Error al instalar la fuente."
+        exit 1
+    fi
+}
 
 # Función para descargar y descomprimir archivos
 download_and_extract() {
@@ -21,7 +64,7 @@ download_and_extract() {
     local file_name=$3
 
     echo "Descargando el archivo desde $url..."
-    wget $url -O $file_name
+    wget -q --show-progress $url -O $file_name
 
     if [ $? -ne 0 ]; then
         echo "Error: No se pudo descargar el archivo desde $url."
@@ -45,6 +88,9 @@ download_and_extract() {
 
     rm $file_name
 }
+
+# Descargar y mover la fuente
+download_and_move_font $URL_FONT $DOWNLOAD_DIR $FONT_DIR $FONT_FILE
 
 # Descargar y descomprimir el tema
 download_and_extract $URL_THEME $DEST_DIR_THEME $FILE_NAME_THEME

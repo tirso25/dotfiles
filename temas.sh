0b104b82fdaf04117b3f89dbda1043a881ae04b2
T25 <106699712+tirso25@users.noreply.github.com>
2024-06-16 02:18:46 +0200

    Update temas.sh

--- temas.sh
+++ temas.sh
@@ -3,6 +3,7 @@
 # URLs de los archivos en el repositorio de GitHub
 URL_THEME="https://github.com/tirso25/dotfiles/raw/main/Nordic-darker.tar.xz"
 URL_ICONS="https://github.com/tirso25/dotfiles/raw/main/Tela-black.tar.xz"
+URL_DRACULA_ICONS="https://github.com/tirso25/dotfiles/raw/main/dracula-icons-main.zip"
 
 # Directorios destino
 DEST_DIR_THEME="/usr/share/themes"
@@ -11,62 +12,47 @@ DEST_DIR_ICONS="/usr/share/icons"
 # Nombres de los archivos descargados
 FILE_NAME_THEME="Nordic-darker.tar.xz"
 FILE_NAME_ICONS="Tela-black.tar.xz"
+FILE_NAME_DRACULA_ICONS="dracula-icons-main.zip"
 
-# Descargar el archivo del tema
-echo "Descargando el archivo del tema desde GitHub..."
-wget $URL_THEME -O $FILE_NAME_THEME
+# Función para descargar y descomprimir archivos
+download_and_extract() {
+    local url=$1
+    local dest_dir=$2
+    local file_name=$3
 
-# Verificar si la descarga del tema fue exitosa
-if [ $? -ne 0 ]; then
-    echo "Error: No se pudo descargar el archivo del tema."
-    exit 1
-fi
+    echo "Descargando el archivo desde $url..."
+    wget $url -O $file_name
 
-# Descargar el archivo de los iconos
-echo "Descargando el archivo de los iconos desde GitHub..."
-wget $URL_ICONS -O $FILE_NAME_ICONS
+    if [ $? -ne 0 ]; then
+        echo "Error: No se pudo descargar el archivo desde $url."
+        exit 1
+    fi
 
-# Verificar si la descarga de los iconos fue exitosa
-if [ $? -ne 0 ]; then
-    echo "Error: No se pudo descargar el archivo de los iconos."
-    exit 1
-fi
+    echo "Descomprimiendo el archivo en $dest_dir..."
+    if [[ $file_name == *.tar.xz ]]; then
+        sudo tar -xf $file_name -C $dest_dir
+    elif [[ $file_name == *.zip ]]; then
+        sudo unzip -q $file_name -d $dest_dir
+    else
+        echo "Error: Formato de archivo no soportado para $file_name."
+        exit 1
+    fi
 
-# Crear el directorio destino del tema si no existe
-if [ ! -d "$DEST_DIR_THEME" ]; then
-    echo "Creando el directorio $DEST_DIR_THEME..."
-    sudo mkdir -p $DEST_DIR_THEME
-fi
+    if [ $? -ne 0 ]; then
+        echo "Error: No se pudo descomprimir el archivo $file_name."
+        exit 1
+    fi
 
-# Crear el directorio destino de los iconos si no existe
-if [ ! -d "$DEST_DIR_ICONS" ]; then
-    echo "Creando el directorio $DEST_DIR_ICONS..."
-    sudo mkdir -p $DEST_DIR_ICONS
-fi
+    rm $file_name
+}
 
-# Descomprimir el archivo del tema en el directorio destino
-echo "Descomprimiendo el archivo del tema en $DEST_DIR_THEME..."
-sudo tar -xf $FILE_NAME_THEME -C $DEST_DIR_THEME
+# Descargar y descomprimir el tema
+download_and_extract $URL_THEME $DEST_DIR_THEME $FILE_NAME_THEME
 
-# Verificar si la descompresión del tema fue exitosa
-if [ $? -ne 0 ]; then
-    echo "Error: No se pudo descomprimir el archivo del tema."
-    exit 1
-fi
+# Descargar y descomprimir los iconos
+download_and_extract $URL_ICONS $DEST_DIR_ICONS $FILE_NAME_ICONS
 
-# Descomprimir el archivo de los iconos en el directorio destino
-echo "Descomprimiendo el archivo de los iconos en $DEST_DIR_ICONS..."
-sudo tar -xf $FILE_NAME_ICONS -C $DEST_DIR_ICONS
+# Descargar y descomprimir los iconos Dracula
+download_and_extract $URL_DRACULA_ICONS $DEST_DIR_ICONS $FILE_NAME_DRACULA_ICONS
 
-# Verificar si la descompresión de los iconos fue exitosa
-if [ $? -ne 0 ]; then
-    echo "Error: No se pudo descomprimir el archivo de los iconos."
-    exit 1
-fi
-
-# Eliminar los archivos descargados
-rm $FILE_NAME_THEME
-rm $FILE_NAME_ICONS
-
-echo "Tema instalado exitosamente en $DEST_DIR_THEME."
-echo "Iconos instalados exitosamente en $DEST_DIR_ICONS."
+echo "Instalación completada."
